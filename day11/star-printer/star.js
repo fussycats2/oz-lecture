32685c43fe23722986ca79e80c0a2d04d027be9a
fussycats2 <fussycats2@naver.com>
2026-03-13 17:45:11 +0900

    star.js

--- day11/star-printer/star.js
+++ day11/star-printer/star.js
@@ -1,5 +1,7 @@
 
 const STAR = "*";
+const minStars = 1;
+const maxStars = 10;
 
 const getPromptInput = () => {
     let input;
@@ -8,17 +10,55 @@ const getPromptInput = () => {
     while (isNotValid) {
         let inputStr = prompt("출력할 별 갯수를 입력하세요.")
         input = Number(inputStr);
-        if(isNaN(input)) {
-            console.log('오류');
-            continue;
+        if(isNaN(input) || input < minStars || input > maxStars) {
+            console.log('Invalid input! Enter a number between 1 and 10.');
+            return null;
         }
+        isNotValid = false;
     }
     return input;
 }
 
-function printStar() {
-    // 별찍기
+const input = getPromptInput();
+
+function printStar(count) {
+    let starMk = "";
+    for (let i = 0; i < count; i++) {
+        starMk += STAR;
+    }
+    console.log(starMk);
 }
 
-const input = getPromptInput();
-printStar(input);
+const printReverseStars = function(count) {
+    for (let i = count; i > 0; i--) {
+        let reverseStarMk = "";
+        for (let j = 0; i > j; j++) {
+        reverseStarMk += STAR;
+        }
+        console.log(reverseStarMk);
+    }
+}
+
+const printSquare = (count) => {
+    for (let i = 0; i < count; i++) {
+        let squareStarMk = "";
+        for (let j = 0; count > j; j++) {
+        squareStarMk += STAR;
+        }
+        console.log(squareStarMk);
+    }
+}    
+
+if (input !== null) {
+    printStar(input);
+}
+console.log("====================")
+
+if (input !== null) {
+    printReverseStars(input);
+}
+console.log("====================")
+
+if (input !== null) {
+    printSquare(input);
+}
