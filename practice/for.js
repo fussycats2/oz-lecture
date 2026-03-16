cdb79e546c3aeb07f9cfee7ecdd3b22607b9b495
fussycats2 <fussycats2@naver.com>
2026-03-16 09:56:27 +0900

    practice

---
+++ practice/for.js
@@ -0,0 +1,25 @@
+for(var i = 0; i < 5; i++) {
+    console.log('javascript');
+}
+
+for(var i = 0; i < 10; i++) {
+    console.log('*');
+}
+
+for(var i = 1; i < 13; i++) {
+    console.log(i + '월');
+}
+
+for(var i = 2; i < 101; i += 2) {
+    console.log()
+}
+
+var sum = 0;
+for(var i = 1; i < 11; i++) {
+    sum = sum + 1;
+}
+console.log(sum);
+
+for(var i = 2; i < 10; i++) {
+    console.log(`2 * ${i} = ${2 * i}`)
+}
